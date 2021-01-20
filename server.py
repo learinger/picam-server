281d83193932a0a37cad162f5d0c8cb28b0b8035
rohbot <rohanoahmad@gmail.com>
2021-01-20 23:27:09 +1100

    init commit

---
+++ server.py
@@ -0,0 +1,68 @@
+#Modified by smartbuilds.io
+#Date: 27.09.20
+#Desc: This web application serves a motion JPEG stream
+# main.py
+# import the necessary packages
+from flask import Flask, render_template, Response, jsonify
+import cv2
+from imutils.video.pivideostream import PiVideoStream
+import imutils
+import time
+import numpy as np
+import time
+
+
+class VideoCamera(object):
+    def __init__(self, flip = False):
+        self.vs = PiVideoStream().start()
+        self.flip = flip
+        time.sleep(2.0)
+
+    def __del__(self):
+        self.vs.stop()
+
+    def flip_if_needed(self, frame):
+        if self.flip:
+            return np.flip(frame, 0)
+        return frame
+
+    def get_frame(self):
+        frame = self.flip_if_needed(self.vs.read())
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+
+pi_camera = VideoCamera(flip=True) # flip pi camera if upside down.
+
+# App Globals (do not edit)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html') #you can customze index.html here
+
+def gen(camera):
+    #get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/photo')
+def photo():
+  frame = pi_camera.get_frame()
+  fname = f'static/{int(time.time())}.jpg'
+  with open(fname, "wb") as f:
+    f.write(frame)
+  return jsonify(photo=fname)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug=False)
+    
+
+
